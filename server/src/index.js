48e3ef59e0e3023d000ae2370b07e058121349f7
Aashish Raj <113645205+aashishrajdev@users.noreply.github.com>
2025-12-01 14:48:29 +0530

    auth-setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
+import { toNodeHandler, fromNodeHeaders } from "better-auth/node";
+import cors from "cors";
+import { auth } from "./lib/auth.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
+
+app.get("api/me", async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  res.json(session);
+});
+
 app.get("/health", (req, res) => {
   res.send("Server is healthy");
 });
 
-app.listen(process.env.port, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
